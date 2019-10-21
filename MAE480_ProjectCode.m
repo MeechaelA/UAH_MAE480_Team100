b0cc7417b2aff7553b1d41263b861a79f60f59e4
jbc0027 <56807516+jbc0027@users.noreply.github.com>
2019-10-20 19:59:02 -0500

    Update MAE480_ProjectCode.m
    Updated

--- MAE480_ProjectCode.m
+++ MAE480_ProjectCode.m
@@ -1,12 +1,26 @@
 %% MAE 480 Project
 % Sarah Nguyen
 % Michael Angeles
+% Jaycie Cleek
 
 %% Problem 1: Pitching moment coefficient for the fuselage
 clear,clc
 % Fuselage Dimensions
 Sections = [1:8]';
 Dx = [10;7;2;12;8.25;8.75;12;3.75];
+
+cy_2 = (7/3)*(9+(2*4.5))/(9+4.5)
+bf_2 = 9 - (2*((9+2*4.5)*(9-4.5))/(6*(9+4.5)))
+
+cy_6 = (8.75/3)*(9+(2*7.214))/(9+7.214)
+bf_6 = 9 - (2*((9+2*7.214)*(9-7.214))/(6*(9+7.214)))
+
+cy_7 = (12/3)*(7.214+(2*4.765))/(7.214+4.765)
+bf_7 = 7.214 - (2*((7.214+2*4.765)*(7.214-4.765))/(6*(7.214+4.765)))
+
+cy_8 = (3.75/3)*(4.765+(2*4))/(4.765+4)
+bf_8 = 4.765 - (2*((4.765+2*4)*(4.765-4))/(6*(4.765+4)))
+
 % bf of trapezoid = d-2((d+2a)(d-a)/6(d+a))
 bf = [4.5;5.841;9;9;9;8.14;6.07;4.39];
 x1 = [14;5.11;2;0;4.125;12.464;22.591;30.82];
@@ -45,9 +59,17 @@ deuda6 = (x1_lh(2)*(1-Deda))-1;
 deuda7 = (x1_lh(3)*(1-Deda))-1;
 deuda8 = (x1_lh(4)*(1-Deda))-1;
 
-% fplot(@(x1_cre) -0.0442*x1_cre.^3 + 0.2946*x1_cre.^2 - 0.6771*x1_cre + 0.6100)
+% fplot(@(x1_cre) -0.0442*x1_cre.^3 + 0.2946*x1_cre.^2 - 0.6771*x1_cre + 0.6100,'b-')
 % xlabel('$\frac{x_{1}}{c_{re}}$','Interpreter','latex','FontSize',15); ylabel('$\frac{d_{\varepsilon}}{d_{\alpha}}$','Interpreter','latex','FontSize',15)
+
+
 % ylim([0 1]); xlim([0 1])
+% fplot(@(x1_cre) -6.4815*x1_cre.^3 + 15.5556*x1_cre.^2 - 13.0833*x1_cre ...
+%            + 4.7593,'b-');
+% xlabel('$\frac{x_{1}}{c_{re}}$','Interpreter','latex','FontSize',15); ylabel('$\frac{d_{\varepsilon}}{d_{\alpha}}$','Interpreter','latex','FontSize',15)
+% hold on 
+% plot(0.166667,deuda3,'r*')
+% legend('Given Function','section 3')
 
 deuda = [deuda1;deuda2;deuda3;0;deuda5;deuda6;deuda7;deuda8];
 
@@ -57,7 +79,7 @@ ctw = 6.61;
 b = 62;
 lambdaw = ctw/crw;
 cbarw = (2/3)*crw*(1+lambdaw+lambdaw^2)/(1+lambdaw);
-S = crw*(b/2)*(1+lambdaw);
+S = 605.288;
 
 % Find Cmaf
 for i = bf(1:8)
@@ -69,7 +91,7 @@ for i = bf(1:8)
         k = k;
     end
     l = i.*j.*k;
-    m = sum(l);
+    m = sum(l)-l(4)
     Cmaf = (pi()/(2*S*cbarw))*m;
 end
 
@@ -126,18 +148,15 @@ k_Main = k(ao_Main);
 % k_Horz = k(ao_Horz);
 % k_Vert = k(ao_Vert);
 
-Area_Main = 576.9%-112.12;    %in2
+Area_Main = 605.288;    %in2
 Area_Horz = 204;       %in2
-% Area_Vert = 115.289;   %in2
 
 b_Main = 62;
 b_Horz = 23;
-% b_Vert = 10;
 
 AR = @(b,S) b^2/S;
 AR_Main = AR(b_Main, Area_Main)
 AR_Horz = AR(b_Horz, Area_Horz);
-% AR_Vert = AR(b_Vert, Area_Vert);
 
 aW = @(AR,k,midcs) ((2*pi)*AR)/(2+sqrt(((AR^2)*(Beta^2)/(k^2))*(1+(((tan(midcs))^2)/Beta))+4))
 aW_Main = aW(AR_Main,k_Main,main_midcs)
@@ -154,14 +173,14 @@ Cr = 12;
 Ct = 4;
 b = 23;
 lambda = Ct/Cr;
-S = Cr*(b/2)*(1+lambda)
+S = 204;
 AR = b^2/S
 % NACA 00-0XX8
 a = sqrt(Gamma*R*T)*3.28084;      % Speed of Sound [ft/s]
 M = V/a;                  % Mach Number
 Beta = sqrt(1-M^2);
 
-%ta = sqrt(1-M^2); Function to solve for aw
+% Function to solve for aw
 ao = @(ao_theory,ao_O_ao_theory,M) (1.05/sqrt(1-M^2))*(ao_O_ao_theory)*(ao_theory);
 k = @(ao) ao/(2*pi);
 at = @(AR,k,midcs) ((2*pi)*AR)/(2+sqrt(((AR^2)*(Beta^2)/(k^2))*(1+(((tan(midcs))^2)/Beta))+4))
@@ -186,36 +205,35 @@ rho_ssl = 0.07651; %lb/ft^3
 
 de_o_da_tail = 2*aW_tail/(pi*AR_Horz);
 
-
 %C_fw = fig3_24(c_bar_t_sum,M,1000000);
 C_fw = 0.045;
 L = 2;
 Main_Lambda = 0.124;
 
 R_LS = @(Lam_LE_w) -1.0400*cos(Lam_LE_w)^2 + 2.0600*cos(Lam_LE_w) + 0.0400;     % Fig 3.25
-R_LS_Wing = R_LS((deg2rad(Main_Lambda)));
+R_LS_Wing = R_LS((deg2rad(lambda)));
 
 Swet_o_S = (2*Area_Main)/Area_Main;
 
-C_Do_w = C_fw*(1+L+100*0.124^4)*R_LS_Wing*Swet_o_S;
+C_Do_w = C_fw*(1+L*0.124+100*0.124^4)*R_LS_Wing*Swet_o_S;
 
 dq_o_q = 2.42*(sqrt(C_Do_w))/((lh/cbarw)+0.3);
 
 neta = 1-dq_o_q;
 
-lift_curve_slope = aW_Main+aW_tail*(1-de_o_da_tail)*neta*(Area_Horz/Area_Main);
+lift_curve_slope = aW_Main+aW_tail*(1-deg2rad(1)-de_o_da_tail)*neta*(Area_Horz/Area_Main);
 
+%% Problem 5
 lt = 18.254;
+bar_v1 = (Area_Horz*lt)/(Area_Main*cbarw)
 
-
-bar_v1 = (Area_Horz*lt)/(Area_Main*cbarw);
 x_cg_bar = (13.06)/cbarw;
 x_bar_ac_wb = 0.25;
-
 x_bar_a = x_cg_bar-x_bar_ac_wb;
 
-dcm_o_da = aW_Main*x_bar_a+Cmaf-aW_tail*(1-de_o_da_tail)*neta*(Area_Horz/Area_Main);
+dcm_o_da = aW_Main*x_bar_a+Cmaf-aW_tail*(1-deg2rad(1)-de_o_da_tail)*neta*bar_v1
 
-N_O =  x_bar_ac_wb + (Cmaf/aW_Main) - (aW_tail/aW_Main)*(1-de_o_da_tail)*bar_v1*neta
+%% Problem 6
+N_O =  x_bar_ac_wb - (Cmaf/lift_curve_slope) + (aW_tail/aW_Main)*(1-deg2rad(1)-de_o_da_tail)*bar_v1*neta
 
 H = N_O - x_cg_bar
